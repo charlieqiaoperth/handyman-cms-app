d8192a1958310ec7383e8520a660170d50422ea0
silverwind <charlieqiaoperth@gmail.com>
2019-06-23 22:01:43 +0800

    try1

--- src/pages/helloworld.js
+++ src/pages/helloworld.js
@@ -1,3 +1,25 @@
+import { Card } from 'antd';
+
 export default () => {
-    return <div>hello world</div>;
-  }
+    const style = {
+        width: '400px',
+        margin: '30px',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+        border: '1px solid #e8e8e8',
+      };
+    
+      return (
+        <Card style={style} actions={[<a>EDIT</a>, <a>DELETE</a>]}>
+          <Card.Meta
+            avatar={<img 
+            alt=""
+            style={{ width: '64px', height: '64px', borderRadius: '32px' }}
+            src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+                />}
+            // title="order"
+            // description="one order for handyman business"
+            />
+            <p>ixixihahh</p>
+        </Card>
+      );
+    }
